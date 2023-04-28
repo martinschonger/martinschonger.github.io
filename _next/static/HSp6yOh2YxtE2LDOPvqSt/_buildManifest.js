ed0dd9c4455d610f7520f3fab943d4100b1d74d1
martinschonger <martin@schonger.dev>
2023-04-28 22:28:17 +0000

    Deploying to gh-pages from @ martinschonger/martinschonger.github.io@47c01dbcaed81b98b6f1046662f398cf36f4dd3e 🚀

--- _next/static/HSp6yOh2YxtE2LDOPvqSt/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/1e18469e8b6a1a54.css","static/chunks/pages/index-82b9fdeaf1691224.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
