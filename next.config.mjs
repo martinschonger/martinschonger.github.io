d03ee63722b23ddeb47ad6b93ac3450adac75a61
Martin Schonger <martin@schonger.dev>
2023-04-28 21:55:47 +0200

    Re-init page.

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  reactStrictMode: true,
   images: {
     loader: 'imgix',
     path: 'sdv.imgix.net',
   },
   assetPrefix: './',
   output: 'export',
-};
+}
 
-export default nextConfig;
+export default nextConfig
