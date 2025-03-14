d0380681284e907bfcac2bbcb2f091f76c315801
martinschonger <martin@schonger.dev>
2025-03-14 12:23:37 +0000

    Deploying to gh-pages from @ martinschonger/martinschonger.github.io@a40578e950cec2a8d5d865b4d5d4ca4ea326ffde 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,19 +16,12 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
+        },{id: "nav-code",
+          title: "code",
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/projects/";
-          },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
+            window.location.href = "/code/";
           },
         },{id: "nav-cv",
           title: "cv",
@@ -37,40 +30,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "projects-project-1",
-          title: 'project 1',
-          description: "project description",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project.html";
-            },},{
-        id: 'social-email',
-        title: 'email',
-        section: 'Socials',
-        handler: () => {
-          window.open("mailto:%6D%61%72%74%69%6E@%73%63%68%6F%6E%67%65%72.%64%65%76", "_blank");
-        },
-      },{
-        id: 'social-github',
-        title: 'GitHub',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://github.com/martinschonger", "_blank");
-        },
-      },{
-        id: 'social-linkedin',
-        title: 'LinkedIn',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.linkedin.com/in/martinschonger", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=WIJa3nMAAAAJ", "_blank");
-        },
-      },{
+        },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
